ce02ed5fe9032afd1dc425d3cce26b806d45d825
Sept <sszheng3@gmail.com>
2023-02-09 15:25:12 -0500

    added the code for cnn

--- optimize.py
+++ optimize.py
@@ -2,8 +2,15 @@ from PIL import Image
 import os
 
 def optimize_image(image_path):
-    image = Image.open(image_path)
-    image.save(image_path, optimize=True, quality=95)
+    try:
+        image = Image.open(image_path)
+        im2 = image.convert('RGB')
+
+        print(f"optimize image '{image_path}'")
+        image.save(image_path, optimize=True, quality=80)
+    except Exception as e:
+        print(f"Failed to download image '{image_path}': {e}")
+        os.remove(image_path)
 
 folder = '/Users/shuai/PycharmProjects/Content-Moderation-for-Social-Media/images/neutral'
 for filename in os.listdir(folder):
